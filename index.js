4af2e4341997b57288d6571f7f1ba7870f687844
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-01 12:52:19 -0300

    inserido primeiro chart do litle gáfico, falta inserir mudanças

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { select, selectAll } from "d3-selection";
 import { csv } from "d3-fetch";
 import { extent } from "d3-array";
-import { scaleLinear } from "d3-scale";
+import { scaleLinear, scaleBand } from "d3-scale";
 import { axisLeft, axisRight, axisBottom, axisTop } from "d3-axis";
 import { line, curveNatural } from "d3-shape";
 import { randomLogNormal } from "d3";
@@ -14,9 +14,9 @@ select("div.chart")
 
 select("div.chart-line-small")
   .append("svg")
-  .attr("class", "line-graph")
+  .attr("class", "line-graph-small")
   .attr("height", "200px")
-  .attr("width", "220px");
+  .attr("width", "250px");
 
 select("div.char-histogram")
   .append("svg")
@@ -34,10 +34,11 @@ csv(
   require("./data/189386_422697_bundle_archive/Video_Games_Sales_as_at_22_Dec_2016.csv")
 ).then((data) => {
   lineChart(data);
+  litleLineChart(data);
 });
 
 function lineChart(incomingData) {
-  let dadosMedidos = medindoVendasAnuais(incomingData);
+  let dadosMedidos = medirVendasAnuais(incomingData);
   let minimoMaximoAnoDeLancamento = medirAnoDeLancamento(dadosMedidos);
   let minimoMaximoDeVendas = medirTotalDeVendas(dadosMedidos);
 
@@ -102,7 +103,7 @@ function lineChart(incomingData) {
   });
 }
 
-function medindoVendasAnuais(data) {
+function medirVendasAnuais(data) {
   let dadosMedidos = [];
   data.forEach((element) => {
     let resultado;
@@ -233,7 +234,10 @@ function euSalesLine(xScale, yScale) {
 }
 
 function alterandoLinha(dadosMedidos, sales) {
-  select("path").transition().duration(1000).attr("d", sales(dadosMedidos));
+  select("path.line-globalsales")
+    .transition()
+    .duration(1000)
+    .attr("d", sales(dadosMedidos));
 }
 
 function destacandoButton(alvo, anteriores) {
@@ -241,4 +245,136 @@ function destacandoButton(alvo, anteriores) {
   anteriores.forEach((el) => {
     select(el).classed("selected", false);
   });
+}
+
+// ---------------------------------------------------------------------------//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ---------------------- litle line chart ------------------------------------//
+
+function litleLineChart(incomingData) {
+  let valoresDoObjeto = [];
+  let propriedadesDoObjeto = ["Global", "NA", "EU", "JP"];
+  let dado = medirVendasDasPublicadoras("Capcom", incomingData);
+  let xScale = scaleBand().domain(propriedadesDoObjeto).range([10, 200]);
+  let yScale = scaleLinear().domain([0, dado.global]).range([180, 10]);
+  let xAxis = axisBottom(xScale).tickSize(-180);
+  let yAxis = axisLeft(yScale);
+
+  let everySales = line()
+    .x((d, i) => {
+      return xScale(propriedadesDoObjeto[i]) + 63;
+    })
+    .y((d) => {
+      return yScale(d);
+    });
+
+  select("svg.line-graph-small")
+    .append("g")
+    .attr("class", "line-group")
+    .attr("transform", "translate(40, 180)")
+    .call(xAxis)
+    .select(".domain")
+    .remove();
+
+  select("svg.line-graph-small")
+    .append("g")
+    .attr("class", "line-group")
+    .attr("transform", "translate(40, 0)")
+    .call(yAxis)
+    .select(".domain")
+    .remove();
+
+  for (let property in dado) {
+    if (!isNaN(+dado[property])) {
+      valoresDoObjeto.push(dado[property]);
+    }
+  }
+
+  select("svg.line-graph-small")
+    .selectAll("circle")
+    .data(valoresDoObjeto)
+    .enter()
+    .append("circle")
+    .attr("class", "scatterplot")
+    .attr("cx", (d, i) => {
+      return xScale(propriedadesDoObjeto[i]) + 63;
+    })
+    .attr("cy", (d) => {
+      return yScale(d);
+    })
+    .attr("r", 5)
+    .attr("fill", "white");
+
+  everySales.curve(curveNatural);
+
+  select("svg.line-graph-small")
+    .append("path")
+    .attr("class", "line-every-sales")
+    .attr("d", everySales(valoresDoObjeto));
+
+  selecionarPublicadora(
+    incomingData,
+    everySales,
+    valoresDoObjeto,
+    propriedadesDoObjeto
+  );
+}
+
+function selecionarPublicadora(incomingData, everySales, propriedadesDoObjeto) {
+  let selectElement = document.querySelector(".publicadoras");
+  let publicadora = "Capcom";
+  let valoresDoObjeto = [];
+  let dado;
+
+  selectElement.addEventListener("change", (event) => {
+    publicadora = event.target.value;
+    dado = medirVendasDasPublicadoras(publicadora, incomingData);
+
+    for (let property in dado) {
+      if (!isNaN(+dado[property])) {
+        valoresDoObjeto.push(dado[property]);
+      }
+    }
+
+    // select("svg.line-graph-small")
+    //   .selectAll("circle")
+    //   .data(valoresDoObjeto)
+    //   .enter()
+    //   .append("circle")
+    //   .attr("class", "scatterplot")
+    //   .attr("cx", (d, i) => {
+    //     return xScale(propriedadesDoObjeto[i]) + 63;
+    //   })
+    //   .attr("cy", (d) => {
+    //     return yScale(d);
+    //   })
+    //   .attr("r", 5)
+    //   .attr("fill", "white");
+
+    // select(".line-every-sales")
+    //   .transition()
+    //   .duration(1000)
+    //   .attr("d", everySales(valoresDoObjeto));
+  });
+}
+
+function medirVendasDasPublicadoras(publicadora, incomingData) {
+  let resultado = {
+    publicadora: publicadora,
+    global: 0,
+    eu: 0,
+    na: 0,
+    jp: 0,
+  };
+
+  incomingData.forEach((element) => {
+    if (element.Publisher == publicadora) {
+      resultado.global += Math.round(+element.Global_Sales);
+      resultado.na += Math.round(+element.NA_Sales);
+      resultado.eu += Math.round(+element.EU_Sales);
+      resultado.jp += Math.round(+element.JP_Sales);
+    }
+  });
+
+  return resultado;
 }
